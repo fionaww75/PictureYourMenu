324e61cb30f17fb119e447745fb94ea91a484337
fionaww75 <wenjing.fang@essec.edu>
2025-03-04 18:14:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ def upload_image():
     return jsonify({"menu_items": menu_data})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from Render, default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
