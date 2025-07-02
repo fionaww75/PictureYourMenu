b04afb8b2dfa433acdd0910b04778de992839db8
wending-fang <fangwjing@outlook.com>
2025-07-02 21:04:49 +0800

    add page information

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 app.use('/api', dishRoute);
